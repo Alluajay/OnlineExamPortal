619e0025177e17cbb9ae7f26e4c987af41a37f14
Alluajay <alludajay@gmail.com>
2016-09-05 14:24:56 +0530

    config file done.

--- Configure.sql
+++ Configure.sql
@@ -1,23 +1,18 @@
 
---
--- Table structure for table `customers_auth`
---
-
-CREATE TABLE IF NOT EXISTS `customers` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` varchar(100) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `city` varchar(50) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `customers_auth`
+--Creating a database
 --
+
+CREATE DATABASE `quiz_db`;
+
+--
+--selecting the database
+--
+
+USE `quiz_db`;
+
+
 --
 -- Table structure for table `host_user`
 --
--- host/api/config.php
+++ host/api/config.php
@@ -5,6 +5,6 @@
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'quiz');
+define('DB_NAME', 'quiz_db');
 
 ?>
